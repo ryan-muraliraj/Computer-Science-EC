3b3a4f977cc99652c9c806c74ff6dbda7736a659
ryan-muraliraj <ryan_muraliraj@myabcusd.org>
2018-11-25 21:13:27 -0800

    first commit

---
+++ Comp Sci EC Project/Data.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Comp_Sci_EC_Project
+{
+    internal class Data
+    {
+        public string Name { get; set; }
+        public string Value { get; set; }
+    }
+}
